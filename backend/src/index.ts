fd64a99c3edccdc93bf870324499e1db6755c974
tbalint19 <tothbalint719@gmail.com>
2023-08-07 20:50:13 +0200

    delete req

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,34 +23,30 @@ const parse = (data: string): User[] => data
       age: +row.split(",")[2],
     }))
 
-// REST API - GET (method) /api/users (path) -> array
-/* server.get("/api/users", async (req: Request, res: Response) => {
-
-  const userData = await fs.readFile("./database/users.txt", "utf-8")
-
-  const response: User[] = parse(userData)
-
-  res.json(response)
-}) */
+const stringify = (data: User[]): string => data
+    .map(user => `${user.id},${user.name},${user.age}`)
+    .join("\n")
 
 const QuerySchema = z.object({
   name: z.string(),
 })
 
+// REST API - GET (method) /api/users (path) -> array
 // GET /api/users?name=John   /api/users?age=30&name=John  -> array
-/* server.get("/api/users", async (req: Request, res: Response) => {
-
-  const result = QuerySchema.safeParse(req.query)
-  if (!result.success)
-    return res.sendStatus(400)
-  const query = result.data
+server.get("/api/users", async (req: Request, res: Response) => {
 
   const userData = await fs.readFile("./database/users.txt", "utf-8")
   const users = parse(userData)
+
+  const result = QuerySchema.safeParse(req.query)
+  if (!result.success)
+    return res.json(users)
+
+  const query = result.data
   let filteredUsers = users.filter(user => user.name.includes(query.name))
 
   res.json(filteredUsers)
-}) */
+})
 
 // GET /api/users/15 (id!!!!) path variable -> 1 object
 server.get("/api/users/:id", async (req: Request, res: Response) => {
@@ -64,6 +60,18 @@ server.get("/api/users/:id", async (req: Request, res: Response) => {
     return res.sendStatus(404)
 
   res.json(filteredUser)
+})
+
+server.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const id = +req.params.id
+
+  const userData = await fs.readFile("./database/users.txt", "utf-8")
+  const users = parse(userData)
+  let filteredUsers = users.filter(user => user.id !== id)
+
+  await fs.writeFile("./database/users.txt", stringify(filteredUsers), "utf-8")
+
+  res.sendStatus(200)
 })
 
 server.listen(3333)
